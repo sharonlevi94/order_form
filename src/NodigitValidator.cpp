9d8385d41e3ba1dac01f07154d94ef33c4d76bbd
sharonlevi94 <sharonlevy96@gmail.com>
2021-04-19 18:33:58 +0300

    Build succeeded.

--- src/NodigitValidator.cpp
+++ src/NodigitValidator.cpp
@@ -1,11 +1,9 @@
 #include "NoDigitValidator.h"
 #include <ctype.h>
 
-NoDigitValidator::NoDigitValidator() {
+NoDigitValidator::NoDigitValidator() {}
 
-}
-
-std::string NoDigitValidator::isValid(const std::string& idToCheck) const {
+std::string NoDigitValidator::isValid(const std::string & idToCheck) const {
     for (int i = 0; i < idToCheck.length(); ++i) {
         if (!isdigit(idToCheck[i])) {
             return "Can't contain digits";

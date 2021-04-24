e0b26eb1cb23d1738dd102f74270093ef222f098
sharonlevi94 <sharonlevy96@gmail.com>
2021-04-24 19:09:16 +0300

    working

--- include/Form.h
+++ include/Form.h
@@ -3,8 +3,7 @@
 #include "Field.h"
 #include <vector>
 #include "FormValidator.h"
-#include "RoomValidator.h"
-#include "SumValidator.h"
+#include "FieldBase.h"
 using std::vector;
 
 /*-----------------------------------------------------------------------------*/
@@ -13,73 +12,19 @@ public:
 	Form();
 	/*--------------------------------*/
 
-	template <class T>
-	void addField(Field<T>* field) 
-        {this->m_Fields.push_back(field);};
+	void addField(FieldBase*);
 
 	/*--------------------------------*/
 
-	void addValidator(FormValidator* validator) 
-        {this->m_formValidators.push_back(validator);};
+    void addValidator(FormValidator*);
 
     /*--------------------------------*/
 
-    void fillForm(){
-        for (auto field : m_Fields){
-            if (!field->getIsCorrect()) {
-                if (dynamic_cast<Field<int>*>(field)) {
-                    std::cout << dynamic_cast<Field<int>*>(field)->getQuestion(); //prints question
-                    dynamic_cast<Field<int>*>(field)->setAnswer();
-                }
-                if (dynamic_cast<Field<uint32_t>*>(field)) {
-                    std::cout << dynamic_cast<Field<uint32_t>*>(field)->getQuestion(); //prints question
-                    dynamic_cast<Field<uint32_t>*>(field)->setAnswer();
-                }
-                if (dynamic_cast<Field<std::string>*>(field)) {
-                    std::cout << dynamic_cast<Field<std::string>*>(field)->getQuestion(); //prints question
-                    dynamic_cast<Field<std::string>*>(field)->setAnswer();
-                }
-            }
-        }
-
-        for (auto form_validator : m_formValidators) {
-            if (!form_validator->getIsCorrect()) {
-                for (auto field : form_validator->getFields()) {
-                    std::cout << dynamic_cast<Field<int>*>(field)->getQuestion(); //prints question
-                    dynamic_cast<Field<int>*>(field)->setAnswer(); //get new answer from user
-                }
-            }
-        }
-    }
+    void fillForm();
 
     /*--------------------------------*/
 	
-    bool validateForm() {
-        this->m_isCorrect = true;
-        for (auto field : m_Fields) {
-            // if matches dynamic cast and field data is not valid:
-            if (dynamic_cast<Field<int>*>(field) and !dynamic_cast<Field<int>*>(field)->getIsCorrect()) {
-                dynamic_cast<Field<int>*>(field)->DoValidation(dynamic_cast<Field<int>*>(field)->getContent());
-            }
-            // if matches dynamic cast and field data is not valid:
-            if (dynamic_cast<Field<uint32_t>*>(field) and !dynamic_cast<Field<uint32_t>*>(field)->getIsCorrect()) {
-                dynamic_cast<Field<uint32_t>*>(field)->DoValidation(dynamic_cast<Field<uint32_t>*>(field)->getContent());
-            }
-            // if matches dynamic cast and field data is not valid:
-            if (dynamic_cast<Field<std::string>*>(field) and !dynamic_cast<Field<std::string>*>(field)->getIsCorrect()) {
-                dynamic_cast<Field<std::string>*>(field)->DoValidation(dynamic_cast<Field<std::string>*>(field)->getContent());
-            }
-            if (!field->getIsCorrect())
-                if (this->m_isCorrect)
-                    this->m_isCorrect = false;
-        }
-
-        for (auto form_validator : m_formValidators) 
-            if (!form_validator->isValid()) 
-                this->m_isCorrect = false;
-           
-        return this->m_isCorrect;
-	};
+    bool validateForm();
 
 	/*--------------------------------*/
 
@@ -92,8 +37,6 @@ public:
 	/*--------------------------------*/
 private:
     vector<FormValidator*> m_formValidators;
-	RoomValidator* m_RoomValidator;
-	SumValidator* m_SumValidator;
 	vector<FieldBase*> m_Fields;
     bool m_isCorrect;
 };
--- src/Form.cpp
+++ src/Form.cpp
@@ -3,15 +3,17 @@
 /*-----------------------------------------------------------------------------*/
 
 Form::Form()
-: m_RoomValidator(nullptr),m_SumValidator(nullptr),m_isCorrect(true){}
+: m_isCorrect(true){}
 
 /*-----------------------------------------------------------------------------*/
 
-vector<FieldBase*> Form::getFields()const { return this->m_Fields; }
+vector<FieldBase*> Form::getFields()const 
+    { return this->m_Fields; }
 
 /*-----------------------------------------------------------------------------*/
 
-vector<FormValidator*> Form::getFormValidators()const { return this->m_formValidators; }
+vector<FormValidator*> Form::getFormValidators()const 
+    { return this->m_formValidators; }
 
 /*-----------------------------------------------------------------------------*/
 
@@ -21,7 +23,77 @@ std::ostream& operator<<(std::ostream& os, const Form& f) {
 
 	for (auto form_validator : f.getFormValidators())
 		if (!form_validator->getIsCorrect())
-			os << form_validator->getErrorMessage();
+			os << form_validator->getErrorMessage() << std::endl;
 	return os;
 }
 /*-----------------------------------------------------------------------------*/
+
+void Form::addValidator(FormValidator* validator)
+    {this->m_formValidators.push_back(validator);}
+
+/*-----------------------------------------------------------------------------*/
+
+void Form::fillForm() {
+    for (auto field : m_Fields) {
+        if (!field->getIsCorrect()) {
+            if (dynamic_cast<Field<int>*>(field)) {
+                std::cout << dynamic_cast<Field<int>*>(field)->getQuestion(); //prints question
+                dynamic_cast<Field<int>*>(field)->setAnswer();
+            }
+            if (dynamic_cast<Field<uint32_t>*>(field)) {
+                std::cout << dynamic_cast<Field<uint32_t>*>(field)->getQuestion(); //prints question
+                dynamic_cast<Field<uint32_t>*>(field)->setAnswer();
+            }
+            if (dynamic_cast<Field<std::string>*>(field)) {
+                std::cout << dynamic_cast<Field<std::string>*>(field)->getQuestion(); //prints question
+                dynamic_cast<Field<std::string>*>(field)->setAnswer();
+            }
+        }
+    }
+
+    for (auto form_validator : m_formValidators) {
+        if (!form_validator->getIsCorrect()) {
+            for (auto field : form_validator->getFields()) {
+                std::cout << dynamic_cast<Field<int>*>(field)->getQuestion(); //prints question
+                dynamic_cast<Field<int>*>(field)->setAnswer(); //get new answer from user
+            }
+        }
+    }
+}
+
+/*-----------------------------------------------------------------------------*/
+
+bool Form::validateForm() {
+    this->m_isCorrect = true;
+    for (auto field : m_Fields) {
+        // if matches dynamic cast and field data is not valid:
+        if (dynamic_cast<Field<int>*>(field) and !dynamic_cast<Field<int>*>(field)->getIsCorrect()) {
+            dynamic_cast<Field<int>*>(field)->DoValidation(dynamic_cast<Field<int>*>(field)->getContent());
+        }
+        // if matches dynamic cast and field data is not valid:
+        if (dynamic_cast<Field<uint32_t>*>(field) and !dynamic_cast<Field<uint32_t>*>(field)->getIsCorrect()) {
+            dynamic_cast<Field<uint32_t>*>(field)->DoValidation(dynamic_cast<Field<uint32_t>*>(field)->getContent());
+        }
+        // if matches dynamic cast and field data is not valid:
+        if (dynamic_cast<Field<std::string>*>(field) and !dynamic_cast<Field<std::string>*>(field)->getIsCorrect()) {
+            dynamic_cast<Field<std::string>*>(field)->DoValidation(dynamic_cast<Field<std::string>*>(field)->getContent());
+        }
+        if (!field->getIsCorrect())
+            if (this->m_isCorrect)
+                this->m_isCorrect = false;
+    }
+
+    for (auto form_validator : m_formValidators)
+        if (!form_validator->isValid())
+            this->m_isCorrect = false;
+
+    return this->m_isCorrect;
+}
+
+/*-----------------------------------------------------------------------------*/
+
+void Form::addField(FieldBase* field)
+{
+    this->m_Fields.push_back(field);
+}
+
